818a42290692c884963517e427fbec84ab080f0c
andochiwa <a1066079469@gmail.com>
2021-06-19 18:26:46 +0900

    custom thread pool -- blocking queue

---
+++ custom-thread-pool/src/com/github/BlockingQueue.java
@@ -0,0 +1,101 @@
+package com.github;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author HAN
+ * @version 1.0
+ * @create 06-19-18:03
+ */
+public class BlockingQueue<T> {
+    // 任务队列
+    private Deque<T> queue = new ArrayDeque<>();
+
+    // 锁
+    private ReentrantLock lock = new ReentrantLock();
+
+    // 生产者条件变量
+    private Condition fullWaitSet = lock.newCondition();
+
+    // 消费者条件变量
+    private Condition emptyWaitSet = lock.newCondition();
+
+    // 容量
+    private int capacity;
+
+    // 带超时的阻塞获取
+    public T poll(long timeout, TimeUnit timeUnit) {
+        lock.lock();
+        try {
+            long nanos = timeUnit.toNanos(timeout);
+
+            while (queue.isEmpty()) {
+                try {
+                    // 返回的是剩余的时间
+                    nanos = emptyWaitSet.awaitNanos(nanos);
+                    if (nanos <= 0) {
+                        return null;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            T t = queue.pollFirst();
+            fullWaitSet.signal();
+            return t;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    // 阻塞获取
+    public T take() {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                try {
+                    emptyWaitSet.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            T t = queue.removeFirst();
+            fullWaitSet.signal();
+            return t;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    // 阻塞添加
+    public void put(T element) {
+        lock.lock();
+        try {
+            while (queue.size() == capacity) {
+                try {
+                    fullWaitSet.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            queue.addLast(element);
+            emptyWaitSet.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    // 获取大小
+    public int size() {
+        lock.lock();
+        try {
+            return queue.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
