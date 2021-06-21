56b218c1fdb211d11b987cb0e75f75bb5c0fa640
andochiwa <a1066079469@gmail.com>
2021-06-22 04:04:04 +0900

    test StampedLock

---
+++ stamped-lock/README.md
@@ -0,0 +1,28 @@
+# StampedLock
+
+该类自 jdk8 加入，是为了进一步优化读性能，它的特点是在使用读锁，写锁时都必须配合`戳`使用
+
+加解读锁：
+
+```java
+long stamp = lock.readLock();
+lock.unlockRead(stamp);
+```
+
+加解写锁：
+
+```java
+long stamp = lock.writeLock();
+lock.unlockWrite(stamp);
+```
+
+StampLock 支持`tryOptimisticRead()`方法（乐观读），读取完毕后需要做一次`戳校验`，如果校验通过，表示这期间没有写操作，数据可以安全使用；如果校验没通过需要重新获取读锁，保证数据安全
+
+```java
+long stamp = lock.tryOptimisticRead();
+// 校验
+if (!lock.validate(stamp)) {
+    // 锁升级
+}
+```
+
---
+++ stamped-lock/src/com/github/StampedLockTest.java
@@ -0,0 +1,71 @@
+package com.github;
+
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * @author HAN
+ * @version 1.0
+ * @create 06-22-3:17
+ */
+public class StampedLockTest {
+    public static void main(String[] args){
+        DataContainerStamper stamper = new DataContainerStamper(1);
+        for (int i = 0; i < 50; i++) {
+            int j = i;
+            new Thread(() -> {
+                try {
+                    if (j == 25) {
+                        stamper.write(5);
+                    }
+                    stamper.read(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
+
+class DataContainerStamper {
+    private int data;
+    private final StampedLock lock = new StampedLock();
+
+    public DataContainerStamper(int data) {
+        this.data = data;
+    }
+
+    public int read(int readTime) throws InterruptedException {
+        long stamp = lock.tryOptimisticRead();
+        System.out.println(Thread.currentThread() + "\toptimistic read locking... " + stamp);
+        Thread.sleep(readTime * 1000L);
+
+        if (lock.validate(stamp)) {
+            System.out.println(Thread.currentThread() + "\tread finish... " + stamp);
+            return data;
+        }
+        System.out.println(Thread.currentThread() + "\tupdating to read lock... " + stamp);
+
+        // 锁升级
+        try {
+            stamp = lock.readLock();
+            System.out.println(Thread.currentThread() + "\tread lock... " + stamp);
+            Thread.sleep(readTime * 1000L);
+            System.out.println(Thread.currentThread() + "\tread finish... " + stamp);
+            return data;
+        } finally {
+            lock.unlockRead(stamp);
+        }
+    }
+
+    public void write(int writeTime) throws InterruptedException {
+        long stamp = lock.writeLock();
+        System.out.println(Thread.currentThread() + "\twrite lock... " + stamp);
+        try {
+            Thread.sleep(writeTime * 1000L);
+            this.data = writeTime;
+        } finally {
+            System.out.println(Thread.currentThread() + "\twrite unlock... " + stamp);
+            lock.unlockWrite(stamp);
+        }
+    }
+}
